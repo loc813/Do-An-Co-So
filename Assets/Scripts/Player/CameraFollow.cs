f0f205e8dc930e6ab89485a5675dfadfe66a9c1c
loc813 <111342133+loc813@users.noreply.github.com>
2024-07-19 16:55:05 +0700

    update background

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target; // Tham chiếu đến nhân vật mà camera sẽ theo dõi
+    [SerializeField] private float smoothing = 0.125f; // Độ mượt khi camera di chuyển
+    [SerializeField] private Vector3 offset; // Khoảng cách giữa camera và nhân vật
+
+    private void FixedUpdate()
+    {
+        if (target == null)
+            return;
+
+        Vector3 desiredPosition = target.position + offset; // Vị trí mong muốn của camera
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothing); // Tính toán vị trí mượt mà
+        transform.position = smoothedPosition; // Cập nhật vị trí của camera
+    }
+}
