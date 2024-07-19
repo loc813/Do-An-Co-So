3af43e97ed3d682d1ab3ca26a778698c274cf74c
loc813 <111342133+loc813@users.noreply.github.com>
2024-07-20 05:23:07 +0700

    update scene fade and menu game

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private SceneController _sceneController;
+    public void Play()
+    {
+        _sceneController.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
